eb2511a4f31f377f6421e1ee24970ec39972b2ff
Simon Work <simonwork@MacBookPro.lan>
2026-03-18 16:48:25 +1300

    change today reading 7

--- assets/js/reading-plan-2026.js
+++ assets/js/reading-plan-2026.js
@@ -15,13 +15,14 @@
 
   var bgNames = {"Ge":"Genesis","Ex":"Exodus","Lev":"Leviticus","Nu":"Numbers","Dt":"Deuteronomy","Jos":"Joshua","Jdg":"Judges","Ru":"Ruth","1Sa":"1+Samuel","2Sa":"2+Samuel","1Ki":"1+Kings","2Ki":"2+Kings","1Ch":"1+Chronicles","2Ch":"2+Chronicles","Ezr":"Ezra","Ne":"Nehemiah","Est":"Esther","Job":"Job","Ps":"Psalms","Pr":"Proverbs","Ecc":"Ecclesiastes","SS":"Song+of+Solomon","Isa":"Isaiah","Jer":"Jeremiah","La":"Lamentations","Eze":"Ezekiel","Da":"Daniel","Hos":"Hosea","Joel":"Joel","Am":"Amos","Ob":"Obadiah","Jnh":"Jonah","Mic":"Micah","Na":"Nahum","Hab":"Habakkuk","Zep":"Zephaniah","Hag":"Haggai","Zec":"Zechariah","Mal":"Malachi","Mt":"Matthew","Mk":"Mark","Lk":"Luke","Jn":"John","Ac":"Acts","Ro":"Romans","1Co":"1+Corinthians","2Co":"2+Corinthians","Gal":"Galatians","Eph":"Ephesians","Php":"Philippians","Col":"Colossians","1Th":"1+Thessalonians","2Th":"2+Thessalonians","1Ti":"1+Timothy","2Ti":"2+Timothy","Tit":"Titus","Phm":"Philemon","Heb":"Hebrews","Jas":"James","1Pe":"1+Peter","2Pe":"2+Peter","1Jn":"1+John","2Jn":"2+John","3Jn":"3+John","Jude":"Jude","Rev":"Revelation"};
 
-  function bgUrl(abbr, chapters) {
+  function bgUrl(abbr, chapters, version) {
     var name = bgNames[abbr];
     if (!name) return null;
     // bgNames may use '+' for internal keys; convert to readable book name
     var book = name.replace(/\+/g, ' ');
     var search = book + (chapters ? ' ' + chapters.replace(/-/g, '-') : '');
-    return 'https://www.biblegateway.com/passage/?search=' + encodeURIComponent(search) + '&version=NIV';
+    var ver = version || 'NIV';
+    return 'https://www.biblegateway.com/passage/?search=' + encodeURIComponent(search) + '&version=' + encodeURIComponent(ver);
   }
 
   function studyQuestionStatus(abbr) {
